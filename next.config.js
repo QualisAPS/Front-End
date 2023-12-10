578423bca5c5bd23bc50d12f0999c8e322a95115
César Augusto <sur.cesar@gmail.com>
2023-12-10 18:42:27 -0300

    env eviroment

--- next.config.js
+++ next.config.js
@@ -4,11 +4,12 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 
 // Remove this if you're not using Fullcalendar features
+const isProd = process.env.ENV === 'production'
 
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
-  assetPrefix:'/',
+  assetPrefix: isProd ? 'http://homologa2.qualisaps.unb.br/admin' : undefined,
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
