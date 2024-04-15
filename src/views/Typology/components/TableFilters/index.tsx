ed5ff4ce1d7bbf00071f3d42fc9081aca2b863e2
gabrielbsb21 <gabrielbsb21@outlook.com>
2024-04-15 16:47:32 -0300

    feat: integration of ubs endpoints, health region, admin region and gsap

--- src/views/Typology/components/TableFilters/index.tsx
+++ src/views/Typology/components/TableFilters/index.tsx
@@ -17,6 +17,41 @@ const TableFilters = ({ setData, tableData }: { setData: any; tableData?: UsersT
   const [role, setRole] = useState<UsersType['role']>('')
   const [plan, setPlan] = useState<UsersType['currentPlan']>('')
   const [status, setStatus] = useState<UsersType['status']>('')
+  const [regiaoSaude, setRegiaoSaude] = useState([])
+  const [gsap, setGsap] = useState([])
+  const [ubs, setUbs] = useState([])
+  const [regiaoAdministrativa, setRegiaoAdministrativa] = useState([])
+  const [error, setError] = useState(null)
+
+  const fetchData = async (endpoint: string, setter: React.Dispatch<React.SetStateAction<any[]>>) => {
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL_V1}/${endpoint}`, {
+        method: 'GET',
+        headers: { 'User-Agent': 'insomnia/2023.5.8' }
+      })
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+
+      const data = await response.json()
+
+      setter(data)
+    } catch (err) {
+      console.error('Fetching error: ', err)
+      setError(err.message)
+    }
+  }
+
+  useEffect(() => {
+    // Chama a função fetchData para cada endpoint
+    fetchData('regiao-saude', setRegiaoSaude)
+    fetchData('gsap', setGsap)
+    fetchData('ubs', setUbs)
+    fetchData('regiao-admin', setRegiaoAdministrativa)
+
+    // Dependências vazias indicam que esse efeito roda apenas uma vez
+  }, [])
 
   useEffect(() => {
     const filteredData = tableData?.filter(user => {
@@ -30,6 +65,8 @@ const TableFilters = ({ setData, tableData }: { setData: any; tableData?: UsersT
     setData(filteredData)
   }, [role, plan, status, tableData, setData])
 
+  console.log(regiaoSaude)
+
   return (
     <CardContent>
       <Grid container spacing={6}>
@@ -38,17 +75,15 @@ const TableFilters = ({ setData, tableData }: { setData: any; tableData?: UsersT
             label='Região de Saúde'
             select
             fullWidth
-            id='select-role'
-            value={role}
-            onChange={e => setRole(e.target.value)}
-            SelectProps={{ displayEmpty: true }}
+            value={role} // Assegure-se que 'role' é o estado correto para este select
+            onChange={e => setRole(e.target.value)} // Atualiza o estado 'role' com o valor selecionado
           >
-            <MenuItem value=''>Todos</MenuItem>
-            <MenuItem value='admin'>Admin</MenuItem>
-            <MenuItem value='author'>Author</MenuItem>
-            <MenuItem value='editor'>Editor</MenuItem>
-            <MenuItem value='maintainer'>Maintainer</MenuItem>
-            <MenuItem value='subscriber'>Subscriber</MenuItem>
+            {regiaoSaude.map(item => (
+              <MenuItem key={item.id} value={item.nome}>
+                {' '}
+                {item.nome}
+              </MenuItem>
+            ))}
           </CustomTextField>
         </Grid>
         <Grid item xs={12} sm={4}>
@@ -61,11 +96,12 @@ const TableFilters = ({ setData, tableData }: { setData: any; tableData?: UsersT
             onChange={e => setPlan(e.target.value)}
             SelectProps={{ displayEmpty: true }}
           >
-            <MenuItem value=''>Todos</MenuItem>
-            <MenuItem value='basic'>Basic</MenuItem>
-            <MenuItem value='company'>Company</MenuItem>
-            <MenuItem value='enterprise'>Enterprise</MenuItem>
-            <MenuItem value='team'>Team</MenuItem>
+            {regiaoAdministrativa.map(item => (
+              <MenuItem key={item.id} value={item.nome}>
+                {' '}
+                {item.nome}
+              </MenuItem>
+            ))}
           </CustomTextField>
         </Grid>
         <Grid item xs={12} sm={4}>
@@ -78,26 +114,30 @@ const TableFilters = ({ setData, tableData }: { setData: any; tableData?: UsersT
             onChange={e => setStatus(e.target.value)}
             SelectProps={{ displayEmpty: true }}
           >
-            <MenuItem value=''>Todos</MenuItem>
-            <MenuItem value='pending'>Pending</MenuItem>
-            <MenuItem value='active'>Active</MenuItem>
-            <MenuItem value='inactive'>Inactive</MenuItem>
+            {gsap.map(item => (
+              <MenuItem key={item.id} value={item.nome}>
+                {' '}
+                {item.nome}
+              </MenuItem>
+            ))}
           </CustomTextField>
         </Grid>
         <Grid item xs={12} sm={4}>
           <CustomTextField
             select
-            label='Unidade Basica de Saúde'
+            label='Unidade Básica de Saúde'
             fullWidth
             id='select-status'
             value={status}
             onChange={e => setStatus(e.target.value)}
             SelectProps={{ displayEmpty: true }}
           >
-            <MenuItem value=''>Todos</MenuItem>
-            <MenuItem value='pending'>Pending</MenuItem>
-            <MenuItem value='active'>Active</MenuItem>
-            <MenuItem value='inactive'>Inactive</MenuItem>
+            {ubs.map(item => (
+              <MenuItem key={item.id} value={item.nome}>
+                {' '}
+                {item.nome}
+              </MenuItem>
+            ))}
           </CustomTextField>
         </Grid>
       </Grid>
